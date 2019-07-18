d386787ef75fb6173218f12c00420504c115864e
leighigoe <leigh@omobono.com>
2019-07-18 09:16:06 +0100

    Updates

---
+++ precache-manifest.55d9f63207837bb49e091ccbe0e3a3fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ae0c3f355c470edd8d1",
+    "url": "/static/css/main.0f9c774f.chunk.css"
+  },
+  {
+    "revision": "1ae0c3f355c470edd8d1",
+    "url": "/static/js/main.df3d78a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "14a531217500da373319",
+    "url": "/static/js/2.6cc87f3c.chunk.js"
+  },
+  {
+    "revision": "a52368c182b3f0bf092b553c2068888a",
+    "url": "/static/media/logo-yellow.a52368c1.svg"
+  },
+  {
+    "revision": "8a0d256ec2e35ec64b455dc295fb6e1f",
+    "url": "/static/media/logo-black.8a0d256e.svg"
+  },
+  {
+    "revision": "62639f944e672bcf2bcef5ef002fae0f",
+    "url": "/index.html"
+  }
+];
