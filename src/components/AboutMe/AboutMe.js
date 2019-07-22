a5118fe79e7a570d8bea395bfb140d8a82a707ce
leighigoe <leigh@omobono.com>
2019-07-22 16:37:23 +0100

    resolving typos in content

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,7 +2,6 @@ import React, {Component, Fragment} from "react";
 import logo from '../../images/logo-black.svg';
 import './AboutMe.scss';
 class AboutMe extends Component {
-
     render(){
         return (
            <Fragment>
@@ -19,18 +18,18 @@ class AboutMe extends Component {
             <div>
               <h1>Leigh Igoe - Developer</h1>
               <h2>Me in a little more detail</h2>
-              <p>I am a driven developer with an eagerness to learn new skills whilst also refining my core progamming knowledge. I’ve had the opportunity to work closely with developers either as inhouse or collaborting with client based teams remotely. I am looking to work within an agile focused team of developers who strive to produce performant and scalable websites/applications.  </p>
+              <p>I am a driven developer with an eagerness to learn new skills whilst also refining my core programming knowledge. I’ve had the opportunity to work closely with developers either as inhouse or collaborating with client-based teams remotely. I am looking to work within an agile focused team of developers who strive to produce performant and scalable websites/applications.  </p>
               <div className="about-me__details">
               <h3 className="about-me__header-divide">Core skills</h3>
               <ul>
-                <li>Develop HTML5 complient code.</li>
-                <li>Strong knowledge of CSS/CSS3, CSS Pre-processors. This includes the implemention of methodologies such as BEM or Atomic design principles. </li>
+                <li>Develop HTML5 compliant code.</li>
+                <li>Strong knowledge of CSS/CSS3, CSS Pre-processors. This includes the implementation of methodologies such as BEM or Atomic design principles.</li>
                 <li>Experience with maintaining pattern libraries for large corporations.</li>
-                <li>A strong knowldge of ES5/ES6.</li>
+                <li>A strong knowledge of ES5/ES6.</li>
                 <li>Experience in developing with React and Angular.</li>
                 <li>Building/setting up projects within Webpack.</li>
-                <li>Building websites using PHP for CMS’s such as Wordpress/ProcessWire/Concrete5.</li>
-                <li>Developing micosites on platforms using Hubspot or Eloqua.</li>
+                <li>Building websites using PHP for CMSs such as WordPress/ProcessWire/Concrete5.</li>
+                <li>Developing microsites on platforms using Hubspot or Eloqua.</li>
                 <li>Using a task runner such as Gulp/Grunt.</li>
                 <li>Experience with using version control systems such as Git. </li>
                 <li>Highly skilled in the use of Adobe software, such as Photoshop, 
@@ -43,6 +42,7 @@ Illustrator and InDesign.</li>
                 <li>Learn and implement tests using Jest and Enzyme.</li>
               </ul>
               </div>
+              <a href="mailto:leigh.igoe@gmail.com" className="btn btn--contrast">Get in touch</a>
 
             </div>
           </div>
