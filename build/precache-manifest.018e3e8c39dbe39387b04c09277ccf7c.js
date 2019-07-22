243b499e8d696835b6f05bab9d2297015d97a9d3
leighigoe <leigh@omobono.com>
2019-07-22 13:43:38 +0100

    update to git ignore

---
+++ build/precache-manifest.018e3e8c39dbe39387b04c09277ccf7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e0479e7abb11a46d2f3",
+    "url": "/static/css/main.21504db1.chunk.css"
+  },
+  {
+    "revision": "2e0479e7abb11a46d2f3",
+    "url": "/static/js/main.63fac66f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "14a531217500da373319",
+    "url": "/static/js/2.6cc87f3c.chunk.js"
+  },
+  {
+    "revision": "a52368c182b3f0bf092b553c2068888a",
+    "url": "/static/media/logo-yellow.a52368c1.svg"
+  },
+  {
+    "revision": "8a0d256ec2e35ec64b455dc295fb6e1f",
+    "url": "/static/media/logo-black.8a0d256e.svg"
+  },
+  {
+    "revision": "eba081c0aafd4097b0edd590b4288b4e",
+    "url": "/index.html"
+  }
+];
